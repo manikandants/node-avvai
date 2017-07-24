0b642fd1622a038bc921a70f8860949243c80d17
manikandanxenon <manikandan@wearexenon.com>
2017-07-25 00:25:30 +0530

    get letters

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const fs = require('fs');
+
+var ezhuthukkalJson = fs.readFileSync(__dirname + '/ezhuthukkal.json', 'utf8');
+
+var tamizhezhuthukkal = JSON.parse(ezhuthukkalJson);
+
+exports.ezhuthukkal = (varthai) => {
+  var ezhuthukkal = [];
+  var sutten = 0;
+  var empty = true;
+  while(sutten < varthai.length) {
+    var ezhuthu = varthai[sutten];
+    console.log(varthai);
+    if ((tamizhezhuthukkal.uyir.indexOf(ezhuthu) >= 0) ||
+      (tamizhezhuthukkal.aidham === ezhuthu) ||
+      (tamizhezhuthukkal.meiAgaram.indexOf(ezhuthu) >= 0) ||
+      (tamizhezhuthukkal.grantham.indexOf(ezhuthu) >= 0)) {
+      ezhuthukkal.push(ezhuthu);
+      empty = false;
+    } else if (tamizhezhuthukkal.uru.indexOf(ezhuthu) >= 0) {
+      if (empty) {
+        ezhuthukkal.push(ezhuthu);
+        empty = false;
+      } else {
+        ezhuthukkal[ezhuthukkal.length - 1] += ezhuthu;
+      }
+    } else {
+      if (ezhuthu.charCodeAt(0) < 256) {
+        ezhuthukkal.push(ezhuthu);
+      } else {
+        if (empty) {
+          ezhuthukkal[ezhuthukkal.length - 1] += ezhuthu;
+        } else {
+          ezhuthukkal.push(ezhuthu);
+          empty = false;
+        }
+      }
+    }
+    sutten++;
+  }
+  return ezhuthukkal;
+};
---
+++ test/ezhuthukkal.js
@@ -0,0 +1,4 @@
+'use strict';
+var avvai = require('../index');
+
+console.log(avvai.ezhuthukkal('அம்மா'));
